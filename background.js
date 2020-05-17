ea458813fe0600b84e49233d1b3168fe8cc71b71
Hans <hansmandersingh1998@gmail.com>
2020-05-17 13:00:45 -0500

    Adds alert box with info about all the movies fetched

--- background.js
+++ background.js
@@ -8,8 +8,21 @@ chrome.contextMenus.create({
     fetch(`https://www.omdbapi.com/?t=${movieName}&apikey=e7468b07`)
       .then(data => data.json())
       .then(json => {
+
+        let message = `Title : ${json.Title}`;
+
         
+        message += newLine;
+        message += `Year : ${json.Year}`;
+        message += newLine;
+        message += `Runtime : ${json.Runtime}`;
+        message += newLine;
+        message += `Genre : ${json.Genre}`;
+        message += newLine;
+        message += `Actors : ${json.Actors}`;
+        message += newLine;
+        message += `IMDb Rating : ${json.imdbRating}`;
+        alert(message);
       })
-    
   }
 });
