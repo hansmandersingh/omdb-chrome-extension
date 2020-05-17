5e742cf64328a0aad189f159d6c246aa81336224
Hans <hansmandersingh1998@gmail.com>
2020-05-17 12:57:23 -0500

    Adds string to teh context menu of chrome and fetches omdb

--- background.js
+++ background.js
@@ -1,1 +1,15 @@
-alert("DID it work");
+chrome.contextMenus.create({
+  'title': 'Search IMDB for "%s"',
+  'contexts': ['selection'],
+  'onclick': (context) => {
+    const movieName = context.selectionText;
+    const newLine = "\r\n"
+
+    fetch(`https://www.omdbapi.com/?t=${movieName}&apikey=e7468b07`)
+      .then(data => data.json())
+      .then(json => {
+        
+      })
+    
+  }
+});
